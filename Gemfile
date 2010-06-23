401463d499df434d914393b939d4df7423c94e7d
TJ Singleton <tjsingleton@vantagestreet.com>
2010-06-22 22:24:16 -0400

    got specs pre-flight ready

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'activerecord', '3.0.0.beta3'
+gem 'sqlite3-ruby'
+
+group :test do
+  gem 'rspec', '2.0.0.beta.11'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,36 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+$LOAD_PATH << File.join(File.dirname(__FILE__))
+
+require "rubygems"
+require "bundler"
+Bundler.setup
+require "active_record"
+require "mood_swing"
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database  => ":memory:"
+)
+
+ActiveRecord::Schema.define do
+    create_table :dogs do |table|
+        table.column :breed, :string
+    end
+end
+
+class Dog < ActiveRecord::Base
+  define_attribute_methods
+  extend MoodSwing
+
+  mood_trigger 'breed'
+
+  def bark
+    puts 'woof'
+  end
+
+  module HoundExtension
+    def bark
+      "Hoooooowl"
+    end
+  end
+end
